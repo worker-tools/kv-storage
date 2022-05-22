0b1d1924243059cceb10be6a64794544c8d83a20
Florian Klampfer <mail@qwtel.com>
2022-05-22 18:54:03 +0700

    Commit

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-import type { StorageArea as IStorageArea, Options } from 'https://ghuc.cc/qwtel/kv-storage-interface/index.d.ts';
+import type { StorageArea as IStorageArea, Options, AllowedKey, Key } from 'https://ghuc.cc/qwtel/kv-storage-interface/index.d.ts';
+export type { Options, AllowedKey, Key }
 
 import * as dn from 'https://ghuc.cc/worker-tools/deno-kv-storage/mod.ts';
 import * as cf from 'https://ghuc.cc/worker-tools/cloudflare-kv-storage/index.ts';
